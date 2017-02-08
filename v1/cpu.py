aea7491964eaea36573adaa8104c8f5b9b141da5
KevinMidboe <kevin.midboe@gmail.com>
2017-02-08 13:35:09 +0100

    Added XML requests for plex, but will be replaced with a api request handling JSON objects instead. Also created cpuTemp for reading linux cpu temps, and added this to app API

--- v1/cpu.py
+++
@@ -1,9 +0,0 @@
-import linuxcpureader
-
-def main():
-	cpu = linuxcpureader.LinuxCpuTemperatureReader()
-	print(cpu)
-	print(cpu.get_reader())
-	print(', '.join("%s: %s" % item for item in cpu.items()))
-
-main() 
