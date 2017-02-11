8464abd097493279c9978ebd4fc7334a25713a6f
Kevin Midboe <kevin.midboe@gmail.com>
2017-02-11 12:09:43 +0100

    Now regexes the output for load command to only get the load values. TODO: Error handling and type checking.

--- status/load.py
+++ status/load.py
@@ -3,8 +3,9 @@ from subprocess import check_output
 from re import findall
 
 def load():
-  arpOutput = check_output("sudo arp-scan -l", shell=True)
-  print(findall('[0-9]{1,2}[\.][0-9]{2}'))
+  arpOutput = check_output("cat /proc/loadavg", shell=True)
+  arpOutput = arpOutput.decode()
+  print(findall('[0-9]{1,2}[\.][0-9]{2}', arpOutput))
 
 if __name__ == '__main__':
 	load()
