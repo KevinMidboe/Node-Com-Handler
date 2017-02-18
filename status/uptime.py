b87bc549bf3657ca72e2c829bf2295454db5d0bd
KevinMidboe <kevin.midboe@gmail.com>
2017-02-18 11:49:48 +0100

    Added more error detection, but need moar

--- status/uptime.py
+++ status/uptime.py
@@ -3,7 +3,7 @@
 # @Author: KevinMidboe
 # @Date:   2017-01-27 19:48:42
 # @Last Modified by:   KevinMidboe
-# @Last Modified time: 2017-02-03 12:33:51
+# @Last Modified time: 2017-02-18 11:49:30
 
 # TODO add better error handling to return statements
 
@@ -14,8 +14,7 @@ def secToDay(seconds):
 	days = int(seconds/86400)
 	if days is 1:
 		return str(days)+' day'
-	else:
-		return str(days)+' days'
+	return str(days)+' days'
 
 def secToHour(seconds):
 	hours = (seconds)//3600
@@ -28,16 +27,15 @@ def timeSinceBoot():
 	currTime = time() 	# Use 'time()' to get seconds currently
 	deltaSeconds = int(currTime-bootTime) 
 
-	# Return in day format
-	if deltaSeconds >= 86400:
-		return secToDay(deltaSeconds)
+	if deltaSeconds >= 86400: # Return in day format
+		uptime = secToDay(deltaSeconds)
 
-	# Return in hour format
 	elif deltaSeconds < 86400 and deltaSeconds >= 0:
-		return secToHour(deltaSeconds)
+		uptime = secToHour(deltaSeconds)
 	else:
-		# TODO Throw error
-		return None
+		return {"errors": "Uptime not found"}
+
+	return {"uptime": uptime}
 
 if __name__=="__main__":
 	print(timeSinceBoot())
